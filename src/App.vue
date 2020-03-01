3cbcbfcbda7fc0be6302af0a3ceb4a2aa20322c9
luxiao <3096888523@qq.com>
2020-03-02 00:37:33 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,128 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="top">
+      <img src alt />
+      <div class="topRight">
+        <el-button-group>
+          <el-button type="primary" @click="register">注册</el-button>
+          <el-button type="primary">登录</el-button>
+        </el-button-group>
+        <div class="topBtn">
+          <el-input v-model="keyWord" placeholder="请输入关键字查询"></el-input>
+          <el-button type="primary" @click="btn">搜索</el-button>
+        </div>
+      </div>
     </div>
-    <router-view/>
+    <div id="nav">
+      <div class="navList">
+        <router-link to="/">首页</router-link>
+        <router-link to="/government">政务公开</router-link>
+        <router-link to="/law">法律服务</router-link>
+        <router-link to="/lawer">律师信息</router-link>
+        <router-link to="/propaganda">普法宣传</router-link>
+        <router-link to="/map">司法地图</router-link>
+        <router-link to="/mailbox">局长信箱</router-link>
+      </div>
+    </div>
+    <router-view />
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      keyWord:''
+    };
+  },
+  methods: {
+    register() {
+      this.$router.push("/register");
+    },
+    btn(){
 
+    }
+  }
+};
+</script>
 <style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+}
+a {
+  text-decoration: none;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background: #f2f2f2;
+  height: 100%;
+  padding-bottom: 10px;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+#top {
+  width: 85%;
+  margin: 0 auto;
+  margin-bottom: 20px;
+  display: flex;
+  justify-content: space-between;
+  padding-top: 20px;
+  .topRight{
+    text-align: right;
+    .el-button-group{
+      .el-button{
+background: #0fa1d4
+      }
+      
     }
   }
+  .topBtn{
+    width: 400px;
+    display: flex;
+    justify-content: space-between;
+    margin-top: 20px;
+    .el-input__inner{
+      border: 1px solid #197fc5;
+      border-top-right-radius: 0;
+      border-bottom-right-radius: 0;
+      border-right: 0
+    }
+    .el-button{
+      border-radius: 0;
+      border-top-right-radius: 4px;
+      border-bottom-right-radius: 4px;
+      background: #1a7ec5;
+      width: 100px
+    }
+  }
+}
+#nav {
+  width: 100%;
+  background: #0ea6d6;
+  font-size: 16px;
+  height: 60px;
+  a {
+    font-weight: bold;
+    // color: #2c3e50;
+    display: flex;
+    width: 14.28%;
+    justify-content: center;
+    align-items: center;
+    font-size: 16px;
+    color: #fff;
+  }
+}
+#nav .router-link-exact-active {
+  color: #fff;
+  background: #58bde0;
+}
+.navList {
+  width: 85%;
+  margin: 0 auto;
+  height: 100%;
+  display: flex;
+  justify-content: space-around;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,44 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/government',
+    name: 'government',
+    component: () => import('../views/government.vue')
+  },
+  {
+    path: '/law',
+    name: 'law',
+    component: () => import('../views/law.vue')
+  },
+  {
+    path: '/lawer',
+    name: 'lawer',
+    component: () => import('../views/lawer.vue')
+  },
+  {
+    path: '/propaganda',
+    name: 'propaganda',
+    component: () => import('../views/propaganda.vue')
+  },
+  {
+    path: '/map',
+    name: 'map',
+    component: () => import('../views/map.vue')
+  },
+  {
+    path: '/mailbox',
+    name: 'mailbox',
+    component: () => import('../views/mailbox.vue')
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: () => import('../views/register.vue')
+  },
+
+
 ]
 
 const router = new VueRouter({
