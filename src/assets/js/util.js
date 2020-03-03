8a821f8cfd729cffdc3c992c73b34c2ba6353bcc
徐泽鹏 <xuzepeng@jc-ai.com>
2020-03-03 22:30:46 +0800

    打包报错

--- src/assets/js/util.js
+++ src/assets/js/util.js
@@ -1,26 +1,4 @@
 const util = {
-  /*
-   例如
-   传入其请求路径"Erp/count/exportCountDepot"
-   请求参数:req = {
-   depot_id:"1",
-   type:1
-   }
-   context:this(vue实例)
-   得到结果http://erp.jcsjzx.cn/Erp/count/exportCountDepot?user_id=86&token=a70398ab4513d5ec7355bd3cd364f442d356770a
-   &type=1&depot_id=1
-   * */
-  getUrlPara (url, req, context) {
-    return url + '?' + delegateObj.geSearchParams(req, context)
-  },
-  geSearchParams (req, context) {
-    console.log(context);
-    let arr = [];
-    Object.keys(req).forEach((key) => {
-      arr.push(key + '=' + req[key])
-    });
-    return arr.join('&')
-  },
   // 递归处理数组tree, 需要保留层级level
   treeHandle(tree = [], level) {
     tree.forEach(item => {
@@ -37,5 +15,4 @@ const util = {
   }
 };
 
-let delegateObj = util;
 export default util;
